f75b3aebc2cf041bf738189e8421c3ab24769cb8
ashutosh pahariya <ashutoshpahariya6@gmail.com>
2021-11-11 19:21:06 +0530

    newassignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,103 @@
+
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My first ever apix!')
+
 });
+
+router.get('/colors', function (req, res) {
+    res.send('this is another api!')
+});
+
+//1-->Create an endpoint for GET /movies that returns a list of movies. 
+//Define an array of movies in your code and return the value in response.
+
+router.get('/movies', function (req, res) {
+    const arr=["DDLJ","Phir Hera Pheri"]
+    res.send(arr)
+});
+
+//2-->Create an endpoint GET movies/indexNumber (For example GET /movies/1 is 
+//a valid request and it should return the movie in your array at index 1). 
+//You can define an array of movies again in your api
+
+//3-->Handle a scenario in problem 2 where if the index is greater than the valid
+// maximum value a message is returned that tells the user to use a valid index in an error message.
+
+router.get('/movies/:index', function (req, res) {
+    const arr=["shole","ram lakhsman","spider man","super man","wonder women"]
+    const val=req.params.index
+    //console.log(val)
+    if(val>arr.length-1 || val<0){
+        res.send("index is out of range")
+    }else{
+    res.send(arr[val])
+    }
+});
+
+
+//4-->Write another api called GET /films. Instead of an array of strings define an array of
+ //movie objects this time. Each movie object should have values - id, name
+ //Return the entire array in this api’s response
+
+router.get('/films', function (req, res) {
+    const arr=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Demo'
+       }]
+    res.send(arr)
+});
+
+
+//5-->Write api GET /films/:filmId where filmId is the value received in request path params.
+ //Use this value to return a movie object with this id. In case there is no such movie present
+ // in the array, return a suitable message in the response body.
+ 
+router.get('/films/:filmId', function (req, res) {
+    const arr=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Demo'
+       }]
+
+    
+     const val=req.params.filmId
+    //const val=Number(req.params.filmId)
+
+    let ind=-1;
+
+    for(let i=0;i<arr.length;i++){       
+        if(arr[i].id == val){
+             ind=i;
+        }
+    } 
+    if(ind !== -1) {
+    res.send(arr[ind])
+    }else{
+        res.send("no movie found") 
+    }
+});
+
 
 module.exports = router;
